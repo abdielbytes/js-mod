85ed970f24530c54c3508bd8c307d59a7d9765e3
abdielbytes <wilsonabdiel86@gmail.com>
2024-11-06 22:03:54 +0100

    wip2

---
+++ js/assigment.js
@@ -0,0 +1,9 @@
+export class Assignment {
+    constructor(id, title, description, dueDate, isCompleted) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.isCompleted = isCompleted;
+    }
+}
